749891e6426ec59d50af9ff6b82a20a183b87274
Segun David <segdavid03@gmail.com>
2024-11-14 07:28:56 +0100

    Gave HTML button elements data-key attributes

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 "use strict";
+
+// getting selectors
+// const operands = document.querySelectorAll(".operand");
+const display = document.querySelector(".display");
+const displayInput = document.querySelector(".display__input");
+const displayOutput = document.querySelector(".display__output");
+const button = document.getElementsByTagName("button");
+console.log(button);
